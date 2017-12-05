87e6a9cf3bbb773411d78a8d1f6e6db91ce7d301
fazzabd <faizabdussalam@gmail.com>
2017-12-05 07:50:35 +0700

    Update TravlendarDB.sql
    tambah create view buatan saya, ada nama location nya

--- Database Design/MySQL Query/TravlendarDB.sql
+++ Database Design/MySQL Query/TravlendarDB.sql
@@ -131,6 +131,27 @@ DELIMITER //
 DELIMITER ;
 
 /*
+create view faiz
+Create or replace view view_event as
+SELECT DISTINCT
+      Tl.traveller_username AS Username, Tl.traveller_fullname AS Fullname,  Tl.traveller_address AS Home_Address,
+      E.event_id AS Event_ID, E.event_name AS Event_Name, E.start_event AS Start_Event, E.end_event AS End_Event, E.note AS Event_Note,
+      L1.location_id as Start_Location_id, L1.location_name AS Start_Location_Event , L2.location_id as End_Location_Id, L2.location_name AS End_Location_Event, 
+	  TM.transportation_name AS Transportation_Mode, T.depature_time AS Depature_Time
+   FROM
+      traveller Tl, event E, location L1, location L2, transportation_mode TM, travel T
+   WHERE
+      Tl.traveller_username = E.traveller_username
+      AND
+      E.event_id = T.event_id
+      AND
+      T.transportation_code = TM.transportation_code
+      And
+      L1.location_id = E.Start_Location_id 
+	  and
+	  L2.location_id = E.End_Location_Id;
+     */
+/*
 
 CREATE VIEW VEVENT AS
    SELECT 
